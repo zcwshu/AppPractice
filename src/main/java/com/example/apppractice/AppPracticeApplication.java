54089399a8f0844422ca46747bfcdbad3b081a48
zcwshu <1107210105@qq.com>
2024-03-20 23:59:26 +0800

    z

---
+++ src/main/java/com/example/apppractice/AppPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.apppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apppractice/AppPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
